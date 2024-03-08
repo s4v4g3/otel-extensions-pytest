fb9c1951f488bb1b7f7f73310c1ea4e13cde415f
Joe Savage <joe.savage@silabs.com>
2024-03-08 14:48:41 -0600

    added test coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import otel_extensions_pytest
 import pytest
+from typing import Union
 
 
 def pytest_addoption(parser):
@@ -12,10 +13,76 @@ def pytest_configure(config):
     otel_extensions_pytest.init_telemetry(config)
 
 
-def pytest_sessionfinish(session, exitstatus):
-    otel_extensions_pytest.pytest_sessionfinish(session, exitstatus)
+@pytest.hookimpl(trylast=True, hookwrapper=True)
+def pytest_runtest_protocol(item, nextitem):
+    g = otel_extensions_pytest.pytest_runtest_protocol(item, nextitem)
+    next(g)
+    yield
+    try:
+        g.send(None)
+    except StopIteration:
+        pass
 
 
-@pytest.hookimpl(trylast=True)
-def pytest_unconfigure(config):
-    otel_extensions_pytest.pytest_unconfigure(config)
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_setup(item):
+    g = otel_extensions_pytest.pytest_runtest_setup(item)
+    next(g)
+    yield
+    try:
+        g.send(None)
+    except StopIteration:
+        pass
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_call(item):
+    g = otel_extensions_pytest.pytest_runtest_call(item)
+    next(g)
+    yield
+    try:
+        g.send(None)
+    except StopIteration:
+        pass
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_teardown(item):
+    g = otel_extensions_pytest.pytest_runtest_teardown(item)
+    next(g)
+    yield
+    try:
+        g.send(None)
+    except StopIteration:
+        pass
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    report = yield
+    g = otel_extensions_pytest.pytest_runtest_makereport(item, call)
+    next(g)
+    try:
+        g.send(report)
+    except StopIteration:
+        pass
+
+
+@pytest.hookimpl(trylast=True, hookwrapper=True)
+def pytest_runtestloop(session):
+    yield
+    otel_extensions_pytest.pytest_sessionfinish(session, 0)
+    otel_extensions_pytest.pytest_unconfigure(session.config)
+
+
+def pytest_exception_interact(
+    node: Union[pytest.Item, pytest.Collector],  # NOSONAR
+    call: pytest.CallInfo,
+    report: Union[pytest.CollectReport, pytest.TestReport],
+):
+    otel_extensions_pytest.pytest_exception_interact(node, call, report)
+
+
+@pytest.hookimpl()
+def pytest_runtest_logreport(report):
+    otel_extensions_pytest.pytest_runtest_logreport(report)
